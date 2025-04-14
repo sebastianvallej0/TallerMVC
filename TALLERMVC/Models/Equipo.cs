8697f59bf315c55b544598c7cb58a18d80945acd
sebastianvallej0 <123136301+sebastianvallej0@users.noreply.github.com>
2025-04-13 22:31:33 -0500

    Add files via upload

---
+++ TALLERMVC/Models/Equipo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taller_LigaPro.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Ingrese el nombre")]
+        public string Nombre { get; set; }
+        [Range(0, 20)] 
+        public int PartidosJugados { get; set; }
+        [Range(0, 20)]
+        public int PartidosGanados { get; set; }
+        [Range(0, 20)]
+        public int PartidosEmpatados { get; set; }
+        [Range(0, 20)]
+        public int PartidosPerdidos { get; set; }
+        public int ToatalPuntos { get; set; }
+    }
+}
