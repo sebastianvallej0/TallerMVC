75db1a5218fd7ca88cba72254752e0cbdebd5c2a
sebastianvallej0 <123136301+sebastianvallej0@users.noreply.github.com>
2025-04-13 22:28:42 -0500

    Add files via upload

---
+++ Taller-LigaPro/Controllers/EquipoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing.Text;
+using Microsoft.AspNetCore.Mvc;
+using Taller_LigaPro.Models;
+using Taller_LigaPro.Repositories;
+
+namespace Taller_LigaPro.Controllers
+{
+    public class EquipoController : Controller  
+    {
+        
+        public IActionResult List()
+        {
+            EquiposRepository repository = new EquiposRepository();
+            var equipos = repository.DevuleveListEquipos();          
+            return View(equipos);
+        }
+        public IActionResult EditarEquipo()
+        {
+            Equipo bsc = new Equipo{ Id = 1 };  
+            return View(bsc);   
+        }
+        
+    }
+}
---
+++ Taller-LigaPro/Repositories/EquiposRepository.cs
@@ -0,0 +1,57 @@
+﻿using Taller_LigaPro.Models;
+
+namespace Taller_LigaPro.Repositories
+{
+    public class EquiposRepository
+    {
+        public IEnumerable<Equipo> DevuleveListEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo bsc = new Equipo
+            {
+                Id = 1,
+                Nombre = "BSC",
+                PartidosJugados = 4,
+                PartidosGanados = 3,
+                PartidosPerdidos = 3,
+                PartidosEmpatados = 5,
+                ToatalPuntos = 0,
+            };
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "LDU",
+                PartidosGanados = 3,
+                PartidosPerdidos = 3,
+                PartidosJugados = 4,
+                PartidosEmpatados = 5,
+                ToatalPuntos = 0,
+            };
+            Equipo eml = new Equipo
+            {
+                Id = 1,
+                Nombre = "Emelec",
+                PartidosGanados = 3,
+                PartidosPerdidos = 3,
+                PartidosJugados = 4,
+                PartidosEmpatados = 5,
+                ToatalPuntos = 0,
+            };
+            Equipo auc = new Equipo
+            {
+                Id = 1,
+                Nombre = "Emelec",
+                PartidosGanados = 3,
+                PartidosPerdidos = 3,
+                PartidosJugados = 4,
+                PartidosEmpatados = 5,
+                ToatalPuntos = 0,
+            };
+            equipos.Add(auc);
+            equipos.Add(bsc);
+            equipos.Add(ldu);
+            equipos.Add(eml);
+            return equipos;
+        }
+    }
+}
