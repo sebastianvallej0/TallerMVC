3ca70c0772a236b5c55d91d44a92d8f900817922
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-04-15 19:11:00 -0500

    Creo modelo Equipo con anotaciones de validación y corrijo error tipográfico

--- TALLERMVC/Models/Equipo.cs
+++ TALLERMVC/Models/Equipo.cs
@@ -7,18 +7,24 @@ namespace Taller_LigaPro.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         [MaxLength(100)]
         [DisplayName("Ingrese el nombre")]
         public string Nombre { get; set; }
-        [Range(0, 20)] 
+
+        [Range(0, 20)]
         public int PartidosJugados { get; set; }
+
         [Range(0, 20)]
         public int PartidosGanados { get; set; }
+
         [Range(0, 20)]
         public int PartidosEmpatados { get; set; }
+
         [Range(0, 20)]
         public int PartidosPerdidos { get; set; }
-        public int ToatalPuntos { get; set; }
+
+        public int TotalPuntos { get; set; } // correcion aqui de la palabra 
     }
 }
